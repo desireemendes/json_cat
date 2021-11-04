c1a6920d4453882121a513b06ecf093ce0f7161e
Desiree Mendes <desireefrost@live.ca>
2021-11-04 13:50:40 -0400

    cat breedFetcher API request

---
+++ breedFetcher.js
@@ -0,0 +1,41 @@
+const request = require('request');
+
+const fetchBreedDescription = function(breedName, cb) {
+  request('https://api.thecatapi.com/v1/breeds', function(requestError, response, body) {
+    let error = null;
+
+    if (requestError) {
+      cb([requestError, response], null);
+      return;
+    }
+
+    if (!breedName) {
+      error = "Invalid";
+      cb(error, null);
+      return;
+    }
+    const data = JSON.parse(body);
+    let catNames = [];
+    for (const cat of data) {
+      catNames.push(cat.name.toLowerCase());
+    }
+
+    breedName = breedName.toLowerCase();
+
+    if (breedName === "list") {
+      const descrription = catNames;
+      cb(null, description);
+      return;
+    }
+    if (catNames.includes(breedName)) {
+      const cats = catNames.indexOf(breedName);
+      const description = data[cats].description;
+      cb(null, description);
+    } else {
+      error = "Breed not found"
+      cb(error, null);
+      return;
+    }
+  });
+};
+
